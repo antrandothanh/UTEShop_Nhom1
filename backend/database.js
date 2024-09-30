8f16ad001fc0076dc10430868006bb8949ec8c25
An Tran <trandothanhan04@gmail.com>
2024-09-30 15:00:34 +0700

    Create database.js and file dotenv in backend

---
+++ backend/database.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const conn = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+})
+
+//Code ở bên dưới
+
+
+
+
+
+
+/* ********************************************* */
