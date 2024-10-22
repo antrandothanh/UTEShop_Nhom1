4b5184a8699b5eb8d0fdf8ac403e0f0a4abd3bbb
antrandothanh <trandothanhan04@gmail.com>
2024-10-22 07:51:42 +0700

    Repair Admin Update Product Page

--- frontend/src/components/AdminUI/AdminUpdateProductPage.js
+++ frontend/src/components/AdminUI/AdminUpdateProductPage.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useLocation } from 'react-router-dom';
 
 function AdminCreateProductPage() {
     const location = useLocation();
     const { product, category } = location.state || {};
     console.log(location);
+
+    const [productName, setProductName] = useState(product.name);
+
+    const handleEditProductName = (productName) => {
+        setProductName(productName);
+    }
 
     return (
         <div className='p-4 sm:ml-64'>
@@ -17,7 +23,7 @@ function AdminCreateProductPage() {
                 <form class="max-w-md mx-auto">
                     <div class="grid md:grid-cols-2 md:gap-6">
                         <div class="relative z-0 w-full mb-5 group">
-                            <input value={product.name} type="text" name="product_name" id="product_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <input onChange={(e) => handleEditProductName(e.target.value)}  value={productName} type="text" name="product_name" id="product_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                             <label for="product_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tên sản phẩm</label>
                         </div>
                         <div class="relative z-0 w-full mb-5 group">
